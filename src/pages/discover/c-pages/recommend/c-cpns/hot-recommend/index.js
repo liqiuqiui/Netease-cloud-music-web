366fcf3533566634071c6c0384ca502223603a7a
qiuqiu <2684196398@qq.com>
2022-02-24 23:43:57 +0800

    fix:优化热门推荐组件

--- src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.js
+++ src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.js
@@ -8,10 +8,10 @@ import LMusicCover from '@/components/music-cover';
 
 export default memo(function LHotRecommend() {
   //state
-
+  
   //redux hooks
   const dispatch = useDispatch();
-  const { hotRecommends } = useSelector(state => ({
+  const {hotRecommends} = useSelector(state => ({
     hotRecommends: state.getIn(['recommend', 'hotRecommends'])
   }), shallowEqual)
   // console.log(hotRecommends);
@@ -20,16 +20,41 @@ export default memo(function LHotRecommend() {
   useEffect(() => {
     dispatch(getHotRecommendsAction(8))
   }, [dispatch])
-  const keywords = [{ title: '华语' }, { title: '流行' }, { title: '摇滚' }, { title: '民谣' }, { title: '电子' }]
+  const keywords = [{
+    text: '华语',
+    to: "/discover/playlist?cate=华语"
+  }, {
+    text: '流行',
+    to: "/discover/playlist?cate=流行"
+  }, {
+    text: '摇滚',
+    to: "/discover/playlist?cate=摇滚"
+  }, {
+    text: '民谣',
+    to: "/discover/playlist?cate=民谣"
+  }, {
+    text: '电子',
+    to: "/discover/playlist?cate=电子"
+  }
+  ]
   //返回的jsx
   return (
     <HotRecommendWrapper>
-      <LThemeHeaderRCm title="热门推荐" keywords={keywords} />
+      <LThemeHeaderRCm
+        more={{text: "更多", to: "/discover/playlist"}}
+        title={{text: "热门推荐", to: "/discover/playlist"}}
+        keywords={keywords}/>
       <div className="recommend-list">
         {
           hotRecommends.map((item, index) => {
             return <div className="recommend-item" key={item.id}>
-              <LMusicCover info={item} />
+              <LMusicCover info={{
+                id: item.id,
+                imgUrl: item.picUrl,
+                playCount: item.playCount,
+                desc: item.name,
+                type: item.type,
+              }}/>
             </div>
           })
         }
