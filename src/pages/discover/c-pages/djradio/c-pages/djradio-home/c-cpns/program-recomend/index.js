464e675c6786e81dc301c2074f13d64de42441ef
qiuqiu <2684196398@qq.com>
2022-02-25 17:59:40 +0800

    feat: 电台首页节目推荐组件

---
+++ src/pages/discover/c-pages/djradio/c-pages/djradio-home/c-cpns/program-recomend/index.js
@@ -0,0 +1,61 @@
+//lib
+import React, { memo, useEffect } from "react";
+import PropTypes from "prop-types";
+
+//components
+import { Wrapper } from "./style.js"
+import ThemeHeaderDj from "../../../../../../../../components/theme-header-dj";
+import { shallowEqual, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+const ProgramRecommend = memo(function ProgramRecommend(props) {
+  const dataList = useSelector(state => state.getIn(["djRadio", "programRecommendList"]).programs, shallowEqual);
+  useEffect(() => {
+    console.log(dataList)
+  }, [dataList])
+  return (
+    <Wrapper>
+      <ThemeHeaderDj
+        title={{
+          text: "推荐节目",
+          to: "/discover/djradio/recommend"
+        }}
+        more={{
+          text: "更多 >",
+          to: "/discover/djradio/recommend"
+        }}
+        showDjText={false}/>
+      <div className="recommend-list">
+        <ul>
+          {
+            dataList?.slice(0, 10)?.map(item => {
+              return (
+                <li key={item.id} className="list-item">
+                  <div className="list-item-left">
+                    <img src={item.coverUrl + "?param=40y40"} alt=""/>
+                  </div>
+                  <div className="list-item-center">
+                    <h3 className="list-item-center-top text-nowrap" title={item.name}>
+                      <Link to={"/discover/djradio/program?id=" + item.id}>{item.name}</Link>
+                    </h3>
+                    <p className="list-item-center-bottom text-nowrap">
+                      <Link to={"/discover/djradio/detail?id=" + item.radio.id}>{item.radio.name}</Link>
+                    </p>
+                  </div>
+                  <Link
+                    to={"/discover/djradio/category?id=" + item.categoryId}
+                    className="list-item-right">{item.radio.category}</Link>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    </Wrapper>
+  )
+});
+
+ProgramRecommend.propTypes = {};
+ProgramRecommend.defaultProps = {};
+
+export default ProgramRecommend;
---
+++ src/pages/discover/c-pages/djradio/c-pages/djradio-home/c-cpns/program-recomend/style.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 426px;
+
+
+  .recommend-list {
+    border: solid #ccc;
+    border-width: 0 1px;
+
+    ul {
+      .list-item {
+        height: 60px;
+        padding: 10px 0 10px 20px;
+        display: flex;
+        column-gap: 10px;
+        align-items: center;
+
+        &:hover {
+          background-color: #eee !important;
+        }
+
+        &:nth-child(even) {
+          background-color: #f7f7f7;
+        }
+
+        &-left {
+          width: 40px;
+          height: 40px;
+        }
+
+        &-center {
+          width: 254px;
+
+          p, h3 {
+            height: 20px;
+            font-size: 12px;
+            line-height: 20px;
+          }
+
+          h3 {
+            a {
+              color: #333;
+            }
+          }
+
+          p {
+            a {
+              color: #999;
+            }
+          }
+        }
+
+        &-right {
+          border: 1px solid #999;
+          color: #999;
+          height: 18px;
+          line-height: 16px;
+          padding: 0 6px;
+          text-decoration: none;
+
+          &:hover {
+            border-color: #666;
+            color: #666;
+          }
+        }
+      }
+    }
+  }
+`;
