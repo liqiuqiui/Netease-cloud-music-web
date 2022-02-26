971bf0834ce0f2d923b9b6d33a39bf2725d050fd
qiuqiu <2684196398@qq.com>
2022-02-26 17:30:03 +0800

    feat: 完成推荐节目页

--- src/pages/discover/c-pages/djradio/c-pages/djradio-recommend/index.js
+++ src/pages/discover/c-pages/djradio/c-pages/djradio-recommend/index.js
@@ -4,11 +4,62 @@ import PropTypes from "prop-types";
 
 //components
 import { Wrapper } from "./style.js"
+import { Link } from "react-router-dom";
+import { shallowEqual, useSelector } from "react-redux";
 
 const DjRadioRecommend = memo(function DjRadioRecommend(props) {
+  const {dataList} = useSelector(state => ({
+    dataList: state.getIn(["djRadio", "programRecommendList"]).programs,
+    
+  }), shallowEqual);
   return (
     <Wrapper>
-      recommend
+      <div className="list-header">
+        <h3 className="title">推荐节目</h3>
+        <span>（每日更新）</span>
+      </div>
+      <div className="list">
+        <ul>
+          {
+            dataList?.map(item => {
+              return (
+                <li key={item.id} className="list-item">
+                  {/*图片*/}
+                  <div className="list-item-image" title="播放">
+                    <img src={item.coverUrl + "?param=40y40"} alt=""/>
+                    <i className="icon sprite_icon"/>
+                  </div>
+                  {/*电台名*/}
+                  <div className="list-item-name">
+                    <h3 className="list-item-center-top text-nowrap" title={item.name}>
+                      <Link to={"/discover/djradio/program?id=" + item.id}>{item.name}</Link>
+                    </h3>
+                  
+                  </div>
+                  {/*描述*/}
+                  <div className="list-item-desc">
+                    <p className="text-nowrap">
+                      <Link
+                        title={item.radio.name}
+                        to={"/discover/djradio/detail?id=" + item.radio.id}>
+                        {item.radio.name}
+                      </Link>
+                    </p>
+                  </div>
+                  {/*类别*/}
+                  <div className="list-item-listener">
+                    播放{item.listenerCount}
+                  </div>
+                  <div className="list-item-like">赞{item.likedCount}</div>
+                  <Link
+                    to={"/discover/djradio/category?id=" + item.categoryId}
+                    className="list-item-cate">{item.radio.category}</Link>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
     </Wrapper>
   )
 });
--- src/pages/discover/c-pages/djradio/c-pages/djradio-recommend/style.js
+++ src/pages/discover/c-pages/djradio/c-pages/djradio-recommend/style.js
@@ -1,5 +1,113 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-	
+  .list-header {
+    height: 42px;
+    border-bottom: 2px solid rgb(194, 12, 12);
+    display: flex;
+
+    .title {
+      font-size: 24px;
+    }
+
+    span {
+      margin: 13px 0 0 10px;
+      color: #999;
+    }
+  }
+
+  .list {
+    border: 1px solid #e2e2e2;
+    border-top: none;
+
+    ul {
+      .list-item {
+        height: 60px;
+        padding: 10px 0 10px 20px;
+        display: flex;
+        column-gap: 10px;
+        align-items: center;
+
+        &:hover {
+          background-color: #eee !important;
+        }
+
+        &:hover .icon {
+          display: block;
+        }
+
+        &:nth-child(even) {
+          background-color: #f7f7f7;
+        }
+
+        &-image {
+          width: 40px;
+          height: 40px;
+          position: relative;
+          cursor: pointer;
+
+          .icon {
+            display: none;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 22px;
+            height: 22px;
+            background-position: 0 -85px;
+          }
+        }
+
+        &-name {
+          width: 304px;
+
+          p, h3 {
+            height: 20px;
+            font-size: 12px;
+            line-height: 20px;
+          }
+
+          h3 {
+            a {
+              color: #333;
+            }
+          }
+
+          p {
+            a {
+              color: #999;
+            }
+          }
+        }
+
+        &-desc {
+          width: 166px;
+        }
+
+        &-listener {
+          color: #999;
+          width: 90px;
+        }
+
+        &-like {
+          color: #999;
+          width: 126px;
+        }
+
+        &-cate {
+          border: 1px solid #999;
+          color: #999;
+          height: 18px;
+          line-height: 16px;
+          padding: 0 6px;
+          text-decoration: none;
+
+          &:hover {
+            border-color: #666;
+            color: #666;
+          }
+        }
+      }
+    }
+  }
 `;
